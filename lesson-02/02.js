94a54bf04ebf833370bcddb825eeeee526e5e80d
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-09 22:23:10 +0300

    add solution less2 - #2

---
+++ lesson-02/02.js
@@ -0,0 +1,15 @@
+function factorial(n) {
+    debugger;
+
+    if (n === 0 || n === 1) {
+        debugger;
+
+        return 1;
+        
+    } else {
+        debugger;
+
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5));
