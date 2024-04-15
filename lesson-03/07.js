fc8180b84390104663beed820b4ce844ee1a37b1
VitalyKorshunov <vitaliy.korshunov1994@gmail.com>
2024-04-15 19:11:00 +0300

    add solution less3 - #7

---
+++ lesson-03/07.js
@@ -0,0 +1,9 @@
+const numbers = [
+    [1, 2, 3, 4, 5],
+    [11, 20, 33, 40, 55],
+    [111, 200, 333, 400, 555],
+];
+  
+const result = numbers.map((item) => item.filter((num) => num % 2 === 0));
+
+console.log(result);
